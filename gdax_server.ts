4cb72e97ede266495098a3a5ba541a5292b1a61e
Serguei Balanovich <sbalanovich@palantir.com>
2017-12-30 17:05:34 -0500

    Added means and regression logic.

--- gdax_server.ts
+++ gdax_server.ts
@@ -22,7 +22,12 @@ let initPrice = 0.0;
 let totalFees = 0.0;
 let lastPrice = NaN;
 let txnSize = .01;
+let projectedMean = 0.0;
 let ticker = 0;
+// let bigChunk = 750000;
+let bigChunk = 62500;
+let iterSpeed = 6250;
+let projectionMultiplier = 1;
 
 GTT.Factories.GDAX.FeedFactory(logger, products).then((feed: GDAXFeed) => {
     feed.on('data', (msg: OrderbookMessage) => {
@@ -32,10 +37,16 @@ GTT.Factories.GDAX.FeedFactory(logger, products).then((feed: GDAXFeed) => {
 
             if (!isNaN(myVol)) {
             	// console.log(myVol)
-                ticker++;
             	vol += myVol
                 trades = trades.concat([parseFloat(msg.origin.price)]);
-                lastTrades = lastTrades.concat([parseFloat(msg.origin.price)]);
+                var myPrice = parseFloat(msg.origin.price)
+                var arr = [];
+                for (var i = 0; i < myVol*100; i++) {
+                    arr.push(myPrice);
+                }
+                lastTrades = lastTrades.concat(arr);
+                ticker+=arr.length;
+                // console.log(lastTrades.length);
                 if (initPrice == 0.0) {
                    initPrice = parseFloat(msg.origin.price);
                 }
@@ -43,22 +54,25 @@ GTT.Factories.GDAX.FeedFactory(logger, products).then((feed: GDAXFeed) => {
 
         }
         // console.log(vol, trades);
-        if (ticker > 30 && vol != lastVol) {
+        if (ticker > iterSpeed && vol != lastVol) {
             lastVol = vol;
         	increment++;
             ticker = 0;
-            var action = ((232 - parseFloat(msg.origin.price))/math.std(lastTrades))*10;
-            if (!isNaN(action)) {
+            var action = ((projectedMean - parseFloat(msg.origin.price))/math.std(lastTrades))*10;
+            if (!isNaN(action) && projectedMean > 0.0) {
                 // if (!((dollars - action < 0) || litecoins + (action / parseFloat(msg.origin.price)))) {
                 dollars -= action;
                 litecoins += (action / parseFloat(msg.origin.price))
                 totalFees += math.abs(.003*action);
             }
-            console.log(`Last: ${msg.origin.price}, Mean: ${math.mean(lastTrades).toFixed(2)}, Std Dev: ${math.std(lastTrades).toFixed(2)}, Trade to make (+buy, -sell): $ ${action}, Txn fee: ${math.abs(.003*action)}`)
+            console.log(`Last: ${msg.origin.price}, Mean: ${math.mean(lastTrades).toFixed(2)}, Projected Mean: ${projectedMean}, Std Dev: ${math.std(lastTrades).toFixed(2)}, Trade to make (+buy, -sell): $ ${action}, Txn fee: ${math.abs(.003*action)}`)
         	console.log(`Total txns: ${(vol).toFixed(0)}, Mean: ${math.mean(trades).toFixed(2)}, Std Dev: ${math.std(trades).toFixed(2)}, Median: ${math.median(trades).toFixed(2)}, Spread: ${(math.max(trades)-math.min(trades)).toFixed(2)} , Spread % (range/min): ${((100*(math.max(trades)-math.min(trades)))/math.min(trades)).toFixed(2)}%`)
             console.log(`Bank: $ ${dollars}, ltc ${litecoins}, total USD: $ ${dollars + litecoins*initPrice}, total fees: $ ${totalFees}`)
-            if (lastTrades.length > 500) {
-                lastTrades.splice(0, 30)
+            if (lastTrades.length > bigChunk) {
+                var runningMeans = makeMeans(lastTrades, iterSpeed);
+                var mb = findLineByLeastSquares(runningMeans);
+                projectedMean = estimateMean(mb[0], lastTrades.length+(iterSpeed*projectionMultiplier), mb[1])
+                lastTrades.splice(0, iterSpeed);
             }
         }
     });
@@ -74,4 +88,85 @@ function printTallies() {
         let tally: string = types.map(t => `${t}: ${tallies[p][t]}`).join('\t');
         console.log(`${p}: ${tally}`);
     }
+}
+
+function stdDev(arr) {
+    var avg = math.mean(arr);
+      
+    var squareDiffs = arr.map(function(value){
+        var diff = value - avg;
+        var sqrDiff = diff * diff;
+        return sqrDiff;
+    });
+  
+    var avgSquareDiff = average(squareDiffs);
+
+    var stdDev2 = Math.sqrt(avgSquareDiff);
+    return stdDev2;
+}
+
+function makeMeans(arr, len) {
+    var runningMeans = [];
+    if (arr.length > len) {
+        for (var i = len; i < arr.length; i++) {
+            runningMeans.push(math.mean(arr.slice(i-len, i)));
+        }
+    }
+    return runningMeans;
+}
+
+function findLineByLeastSquares(values_y) {
+    var values_x = [];
+    for (var i = 1; i < values_y.length+1; i++) {
+        values_x.push(i);
+    }
+    var sum_x = 0;
+    var sum_y = 0;
+    var sum_xy = 0;
+    var sum_xx = 0;
+    var count = 0;
+
+    /*
+     * We'll use those variables for faster read/write access.
+     */
+    var x = 0;
+    var y = 0;
+    var values_length = values_x.length;
+
+    if (values_length != values_y.length) {
+        throw new Error('The parameters values_x and values_y need to have same size!');
+    }
+
+    /*
+     * Nothing to do.
+     */
+    if (values_length === 0) {
+        return [ [], [] ];
+    }
+
+    /*
+     * Calculate the sum for each of the parts necessary.
+     */
+    for (var v = 0; v < values_length; v++) {
+        x = values_x[v];
+        y = values_y[v];
+        sum_x += x;
+        sum_y += y;
+        sum_xx += x*x;
+        sum_xy += x*y;
+        count++;
+    }
+
+    /*
+     * Calculate m and b for the formular:
+     * y = x * m + b
+     */
+    var m = (count*sum_xy - sum_x*sum_y) / (count*sum_xx - sum_x*sum_x);
+    var b = (sum_y/count) - (m*sum_x)/count;
+
+    return [m, b];
+}
+
+function estimateMean(m, x, b) {
+    return (m*x) + b
 }
